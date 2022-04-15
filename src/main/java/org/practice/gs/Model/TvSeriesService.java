7c36d52a1e6c64232037855a9ce411141209553b
Norsi47 <briggsnorsi@yahoo.com>
2022-04-15 00:24:51 -0400

    needs logic inside controller for some reason

--- src/main/java/org/practice/gs/Model/TvSeriesService.java
+++ src/main/java/org/practice/gs/Model/TvSeriesService.java
@@ -1,28 +1,28 @@
-package org.practice.gs.Model;
-
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.practice.gs.TvSeriesClient;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class TvSeriesService {
-
-    @RestClient
-    TvSeriesClient tvSeriesClient;
-
-    //need to make it into an arry list to add objects in it
-    private List <TvSeriesResponse> tvSeriesResponseList = new ArrayList<>();
-
-    public void getTvSeries() {
-        TvSeriesResponse tvSeriesResponseClassName = tvSeriesClient.get("girls");
-        //cant use ".add" without array list
-        tvSeriesResponseList.add(tvSeriesResponseClassName);
-
-    }
-
-
-
-
-
-}
+//package org.practice.gs.Model;
+//
+//import org.eclipse.microprofile.rest.client.inject.RestClient;
+//import org.practice.gs.TvSeriesClient;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class TvSeriesService {
+//
+//    @RestClient
+//    TvSeriesClient tvSeriesClient;
+//
+//    //need to make it into an arry list to add objects in it
+//    private List <TvSeriesResponse> tvSeriesResponseList = new ArrayList<>();
+//
+//    public void getTvSeries() {
+//        TvSeriesResponse tvSeriesResponseClassName = tvSeriesClient.get("girls");
+//        //cant use ".add" without array list
+//        tvSeriesResponseList.add(tvSeriesResponseClassName);
+//
+//    }
+//
+//
+//
+//
+//
+//}
--- src/main/java/org/practice/gs/TvSeriesController.java
+++ src/main/java/org/practice/gs/TvSeriesController.java
@@ -1,25 +1,32 @@
 package org.practice.gs;
 
-import org.practice.gs.Model.TvSeriesService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.practice.gs.Model.TvSeriesResponse;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/pracTvseries")
 public class TvSeriesController {
 
-    @Inject
-    TvSeriesService tvSeriesService;
+    //all logic has to be done here for some reason
+
+    @RestClient
+    TvSeriesClient tvSeriesClient;
+
+    private List<TvSeriesResponse> tvSeriesResponseList = new ArrayList<>();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTvSeries() {
-        tvSeriesService.getTvSeries();
-        return Response.ok(tvSeriesService).build();
+        TvSeriesResponse tvSeriesResponseClassname = tvSeriesClient.get("girls");
+        tvSeriesResponseList.add(tvSeriesResponseClassname);
+        return Response.ok(tvSeriesResponseList).build();
     }
 
 }
